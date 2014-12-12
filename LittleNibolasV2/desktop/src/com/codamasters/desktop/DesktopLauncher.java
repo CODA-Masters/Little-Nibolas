5eceda77c44e0b999944e510b963f009c0d5fa86
Julio Martínez Martínez-Checa <julioxus@gmail.com>
2014-12-12 22:33:57 +0100

    Añadido tamaño de pantalla mas grande

--- LittleNibolasV2/desktop/src/com/codamasters/desktop/DesktopLauncher.java
+++ LittleNibolasV2/desktop/src/com/codamasters/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.codamasters.LittleNibolas;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//config.width = 1280;
-		//config.height = 720;
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new LittleNibolas(), config);
 	}
 }
